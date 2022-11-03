58897d4c6b96a0c7f036b8d3009621b66cf43604
HamsterAPig <102395304+HamsterAPig@users.noreply.github.com>
2022-11-03 23:00:19 +0800

    Feat: 填充屏幕函数

---
+++ OLEDDriver.c
@@ -0,0 +1,105 @@
+/**
+ * @Description OLED驱动程序
+ * @Author jinming xi
+ * @Date 2022/11/3
+ * @Project OLEDDriver
+*/
+#include "OLEDDriver.h"
+#include <stdint.h>
+#include <memory.h>
+
+/**
+ * OLED 写命令函数，作用于仅限于本文件
+ * @param pcmd 命令数组指针
+ * @param total 命令长度
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_WriteCmd(uint8_t *pcmd, uint16_t total);
+
+/**
+ * @breif 定义两个数组
+ * @parm g_oled_buffer作为图形缓存
+ * @parm g_command_buffer作为命令缓存
+ */
+uint8_t g_oled_buffer[OLED_PAGE_SIZE][OLED_PIX_WIDTH];
+uint8_t g_command_buffer[OLED_COMMAND_BUFFER_LENGTH];
+
+/**
+ * OLED初始化函数
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_Init(void) {
+    const uint8_t _ssd1306_init_parm[] = {0xae,0x20,0x00,0xb0,0xc8,0x00,0x10,0x40,
+                                          0x81,0xff,0xa1,0xa6,0xa8,0x3f,0xa4,
+                                          0xd3,0x00,0xd5,0xf0,0xd9,0x22,0xda,
+                                          0x12,0xdb,0x20,0x8d,0x14,0xaf};
+
+}
+
+/**
+ * 将g_oled_buffer里面的内容更新到屏幕中
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_Reflush_GSRAM() {
+    OLED_StatusTypeDef status;
+    const uint8_t _ssd1306_write_command_parm[] = {0xb0,0x00,0x10};
+    status = OLED_WriteCmd(_ssd1306_write_command_parm, 3);
+    if (status != OLED_OK) return status;
+    status = OLED_I2C_Transmit(OLED_PHY_ADDRESS, 0x40, g_oled_buffer, OLED_PIX_WIDTH * OLED_PAGE_SIZE);
+    return status;
+}
+
+/**
+ * 点亮某一个点或者点灭某一个点
+ * @param x 横坐标
+ * @param y 纵坐标
+ * @param state 点的状态，点亮为1，点灭为0
+ * @return OLED Status
+ */
+#define SET_BIT(REG, BIT)     ((REG) |= (BIT))
+#define	CLEAR_BIT(x, bit)	(x &= ~(1 << bit))	/* 清零第bit位 */
+OLED_StatusTypeDef OLED_SetPoint(uint8_t x, uint8_t y, uint8_t state) {
+    OLED_StatusTypeDef status;
+
+    // 检查输入参数.超出范围自动返回错误
+    if (x >= OLED_PIX_WIDTH || y >= OLED_PIX_HEIGHT) return OLED_ERROR;
+
+    uint8_t fill_data = y % 8;
+    uint8_t old_data = g_oled_buffer[(y -fill_data) / 8][x];
+    if (0 == state)
+        g_oled_buffer[(y -fill_data) / 8][x] = CLEAR_BIT(old_data, fill_data);
+    else
+        SET_BIT(g_oled_buffer[(y -fill_data) / 8][x], state << fill_data);
+
+    return OLED_OK;
+}
+
+/**
+ * 将g_oled_buffer里面的内容更新到屏幕中
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_Fill(uint8_t state) {
+    memset(g_oled_buffer, state, sizeof(uint8_t) * OLED_PIX_WIDTH * OLED_PAGE_SIZE);
+    OLED_Reflush_GSRAM();
+}
+
+/**
+ * OLED 写命令函数，作用于仅限于本文件
+ * @param pcmd 命令数组指针
+ * @param total 命令长度
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_WriteCmd(uint8_t *pcmd, uint16_t total) {
+#ifdef __ENABLE_DMA_OLED
+    /**
+    * @note 拷贝一份原有数据防止函数退出后，pcmd指向的地址失效
+    * @note 进而导致DMA发送数据异常
+    */
+    for (int i = 0; i < total; ++i)
+        g_command_buffer[i] = pcmd[i];
+    OLED_StatusTypeDef status = OLED_I2C_Transmit(OLED_PHY_ADDRESS, 0x00, g_command_buffer, total);
+#else
+    //Todo 这里写不用DMA传输的过程
+#endif
+    return status;
+}
---
+++ OLEDDriver.h
@@ -0,0 +1,81 @@
+/**
+ * @Description OLED驱动程序
+ * @Author jinming xi
+ * @Date 2022/11/3
+ * @Project OLEDDriver
+*/
+
+#ifndef OLEDDRIVER_H
+#define OLEDDRIVER_H
+#ifdef __cplusplus
+    extern "C" {
+#endif // C++ Support
+
+#define OLED_PHY_ADDRESS 0x78               // I2C 物理地址
+#define OLED_PIX_WIDTH 128                  // OLED屏幕横向像素
+#define OLED_PIX_HEIGHT 64                  // OLED屏幕纵向像素
+
+#define OLED_PAGE_SIZE OLED_PIX_HEIGHT / 8  // OLED驱动存储页数
+
+#define OLED_COMMAND_BUFFER_LENGTH 32       // OLED 命令存储Buffer长度
+
+/**
+ * @brief 定义返回常量，方便调试的时候判断状态
+ */
+typedef enum
+{
+    OLED_OK       = 0x00U,
+    OLED_ERROR    = 0x01U,
+    OLED_BUSY     = 0x02U,
+    OLED_TIMEOUT  = 0x03U
+} OLED_StatusTypeDef;
+
+/**
+ * @brief 使用二维数组存储像素，相当于是画布
+ * @brief 所有对其的修改都不会立即同步到OLED上面
+ * @brief 需要调用刷新函数
+ */
+extern uint8_t g_oled_buffer[OLED_PAGE_SIZE][OLED_PIX_WIDTH];
+
+/**
+ * OLED初始化函数
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_Init(void);
+
+/**
+ * I2C传输函数，需要用户自定义
+ * @param DevAddress 设备地址
+ * @param MemAddress 内存地址
+ * @param pData 数据指针
+ * @param Size 数据长度
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_I2C_Transmit(uint16_t DevAddress, uint16_t MemAddress, uint8_t *pData, uint16_t Size);
+
+/**
+ * 将g_oled_buffer里面的内容更新到屏幕中
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_Reflush_GSRAM();
+
+/**
+ * 点亮某一个点或者点灭某一个点
+ * @param x 横坐标
+ * @param y 纵坐标
+ * @param state 点的状态，点亮为1，点灭为0
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_SetPoint(uint8_t x, uint8_t y, uint8_t state);
+
+/**
+ * 全屏填充
+ * @param state 0是清屏,1是点亮
+ * @return OLED Status
+ */
+OLED_StatusTypeDef OLED_Fill(uint8_t state);
+
+#ifdef __cplusplus
+}
+#endif // C++ Support
+#endif //OLEDDRIVER_H
